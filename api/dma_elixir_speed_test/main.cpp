a1c2230dba31403ac2d295c1fc94959e05825523
Kirill <kirill_yustickii@mail.ru>
2022-10-06 16:54:41 +0300

    Update main.cpp

--- api/dma_elixir_speed_test/main.cpp
+++ api/dma_elixir_speed_test/main.cpp
@@ -10,86 +10,17 @@ using namespace std;
 
 int main(int argc, char ** argv)
 {
-//    create_test_file();
-//    exit(0);
     std::string file_name = "/dev/dma_elixir_o_v1";
     int fd;
     int ret;
     int act = 0;
     int i;
-//    char write_buf[BUF_SIZE];
-//    char read_buf[BUF_SIZE];
     struct pollfd fds;
-
-//    for (i = 0; i < BUF_SIZE; i++)
-//        write_buf[i] = (char)i;
-
-//    if (argc < 2)
-//    {
-//        std::cout << "Need to choose: read {1} / write {2}\n";
-//        exit(0);
-//    }
-
-//    /* get the count of transactions */
-//    act = atoi(argv[1]);
 
     /* open device file */
     fd = open_file(file_name, O_RDWR);
     if (fd < 0)
         exit(EXIT_FAILURE);
-
-//    int mask = POLLOUT | POLLIN;
-//    fds.fd = fd;
-//    fds.events = mask;
-
-
-//    while (1)
-//    {
-//        ret = poll(&fds, 1, -1);
-//        if (ret < 0)
-//        {
-//            std::cout << "There is an error in poll function\n";
-//        }
-//        else if (ret == 0)
-//        {
-//            std::cout << "Timeout in poll function occurred\n";
-//        }
-//        else
-//        {
-//            if (fds.revents & POLLOUT)
-//            {
-//                ret = write(fds.fd, write_buf, BUF_SIZE);
-////                if (ret != (BUF_SIZE))
-////                {
-////                    std::cout << "Writed less than needed\n";
-////                    //global_rd_err++;
-////                    return -1;
-////                }
-
-//                //fds.revents = 0;
-//                //sleep(1);
-//            }
-
-//            if (fds.revents & POLLIN)
-//            {
-//                ret = read(fds.fd, read_buf, BUF_SIZE);
-////                if (ret != (BUF_SIZE))
-////                {
-////                    std::cout << "Writed less than needed\n";
-////                    //global_rd_err++;
-////                    return -1;
-////                }
-
-//                //fds.revents = 0;
-//                //sleep(1);
-//            }
-
-//            fds.revents = 0;
-//        }
-//    }
-
-
-
 
     for (int i = 0; i < ITERATION_COUNT; i++)
     {
@@ -105,7 +36,6 @@ int main(int argc, char ** argv)
             std::cout << "Write error" << std::endl;
             exit(1);
         }
-//        sleep(2);
 
         auto t1 = high_resolution_clock::now();
         ret = read(fd, read_buf, BUF_SIZE);
@@ -135,33 +65,5 @@ int main(int argc, char ** argv)
               << m_bits / (compl_time / 1000) << " Mbits/sec\n";
 
     return 0;
-
-
-
-//    switch(act)
-//    {
-//    case 1:
-//        /* read from device file */
-//        ret = read_data(fd);
-//        if (ret)
-//            std::cout << "There is " << ret << " errors in dma read occurred\n";
-//        else
-//            std::cout << "There is no errors by transactions" << std::endl;
-//        break;
-
-//    case 2:
-//        ret = write_data(fd);
-//        if (ret < 0)
-//            std::cout << "There is an error in write function\n";
-//        break;
-
-//    default:
-//        std::cout << "There is no such action\n";
-//        exit(EXIT_FAILURE);
-//    }
-
-//    /* count time */
-
-//    return 0;
 }
 
